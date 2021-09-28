587d3a804723116ca0dfe7760d7627bf3225738f
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-27 22:00:47 -0700

    form question and addition vars

---
+++ Assests/jss.js
@@ -0,0 +1,37 @@
+var questionIndex = [];
+// add index to push to
+var answerIndex = [];
+var correctTotal = [];
+//use array length to total and score
+var timerCount = document.querySelectorAll(timerCount);
+var startBtn = document.querySelectorAll(startBtn);
+
+var scoreTotal = document.querySelectorAll(totalCorrect)
+//establish questions
+var startScreen = document.querySelectorAll(startScreen);
+var endScreen  = document.querySelectorAll(endScreen);
+var nameTrack = document.querySelectorAll(nameTrack);
+var scoreTrack  = document.querySelectorAll(scoreTrack);
+//start and end screen
+// write body of quiz in function, better way to populate?
+
+quizBody = [{
+    qN: 1,
+    question: "place holder?",
+    answers: [
+      "A",
+      "B",
+      "C",
+      "D",
+    ],
+    correct:
+      "A",
+  },
+//place holder question
+]
+
+
+
+
+
+}
