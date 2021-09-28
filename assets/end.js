b581d3bb1d18f40c7ecd4b3f4beeab7dec718bed
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-28 01:07:23 -0700

    restructored files and added multi page feed loop for end and landing page

---
+++ assets/end.js
@@ -0,0 +1,10 @@
+const username = document.querySelector('#scoreName')
+const saveScore = document.querySelector('#saveScore')
+const currentScore = document.querySelector('#currentScore')
+
+const highScore = JSON.parse(localStorage.getItem('currentScore')
+
+scoreName.addEventListener('keyup', )
+
+
+//parse score from local
--- assets/jss.js
+++ assets/jss.js
@@ -11,7 +11,7 @@ let currectAnswers = {}
 let availableQuestion = {}
 let questionsIndex = {}
 //changed array structure
-
+//timer needed! interact with question parse
 quizBody = [{
     qN: 1,
     question: "HTML is responsible for what part of a webpage?",
