c58d809044afd34eb7142b15a02b294adc2f9716
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-28 19:42:49 -0700

    finished unstyled quiz

--- assets/end.js
+++ assets/end.js
@@ -1,7 +1,14 @@
-
 const highScore = JSON.parse(localStorage.getItem('highscores'))
-
-
-
-
+const highScoreText = document.querySelector('#high-score-display')
 //parse score from local
+
+function displayHighScore() {
+    let highScoreText = highScore
+    highScoreText.forEach(element => {
+      let tempScoreDisplay = document.createElement('button')
+      tempScoreDisplay.textContent = element
+      highScoreText.appendChild('highScore')
+    }
+}
+
+displayHighScore()
